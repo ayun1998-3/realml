adda9f842a9b58157bf26e5c6999ebbb19c407b3
ayun1998-3 <77704153+ayun1998-3@users.noreply.github.com>
2022-02-14 14:18:53 -0500

    internalize audio

--- src/data.js
+++ src/data.js
@@ -1,3 +1,50 @@
+export const generateAudio = (func) => {
+    navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+
+        const context = new AudioContext();
+        var analyser = context.createAnalyser();
+        analyser.smoothingTimeConstant = 0.2;
+        analyser.fftSize = 1024;
+        analyser.minDecibels = -127;
+        analyser.maxDecibels = 0;
+
+        var filterNode = context.createBiquadFilter();
+        // filterNode.type = 'highpass';
+        // filterNode.frequency.value = 7000;
+
+        var gainNode = context.createGain(); // Create a gain node to change audio volume.
+        // gainNode.gain.value = 1.0;  
+
+        const microphone = context.createMediaStreamSource(stream);
+        microphone.connect(filterNode);
+        filterNode.connect(gainNode);
+        // microphone.connect(gainNode);
+        gainNode.connect(analyser);
+        analyser.connect(context.destination);
+
+        stream.onended = () => {
+            microphone.disconnect();
+            gainNode.disconnect();
+            filterNode.disconnect()
+        }
+
+        // Show Audio Volume
+        let volumeCallback = null;
+        let volumeInterval = null;
+        const frequencies = new Uint8Array(analyser.frequencyBinCount);
+        let raw = new Uint8Array(analyser.frequencyBinCount)
+        const getData = () => {
+            analyser.getByteFrequencyData(frequencies);
+            analyser.getByteTimeDomainData(raw)
+            func(raw) // perform operation on latest raw audio data
+            console.log(raw, frequencies)
+        };
+
+        setInterval(getData, 100); // Get Data Every 100ms
+
+    })
+}
+
 
 export const simulate = (func) => { //simulates data stream
     // func is a function that takes an Array
@@ -15,7 +62,7 @@ export const simulate = (func) => { //simulates data stream
     }
 
     animate()
-    
+
 }
 
 export const generate = (func, dataArray) => { // takes an array and returns generated data
@@ -30,7 +77,7 @@ export const generate2d = (func, dataArray, length) => { // takes an array and r
     dataArray = dataArray.map(val => func(val))
     let data2d = []
     for (let i = 0; i < dataArray.length; i += length) {
-        data2d.push(dataArray.slice(i, i+length))
+        data2d.push(dataArray.slice(i, i + length))
     }
 
     console.log(data2d)
