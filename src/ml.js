8a9dc07354f5c2715a41cc4a94c5db07342b325d
ayun1998-3 <77704153+ayun1998-3@users.noreply.github.com>
2022-02-25 22:38:10 -0500

    deep learning framework

--- src/ml.js
+++ src/ml.js
@@ -1,3 +1,4 @@
+import * as ml5 from "https://unpkg.com/ml5@latest/dist/ml5.min.js"
 
 export const sort = (o) => {
     console.log(ML)
@@ -21,13 +22,13 @@ export const generateLabels = (classifier, x) => { //takes in classifying functi
 export const generateLabels2d = (classifier, x) => {
 
     return x.map(sample => { //for each sample
-        let i = sample.reduce((a,b) => a + b) / sample.length // calculate mean
+        let i = sample.reduce((a, b) => a + b) / sample.length // calculate mean
         return classifier(i) // return classified result based on mean
     })
 }
 
 export const naiveBayes = (x, y) => {
-    
+
     let model = new ML.NaiveBayes.GaussianNB()
     model.train(x, y)
     return model;
@@ -39,7 +40,7 @@ export const lda = (someValue) => {
 
 }
 
-export const pca = (x, y) =>  {
+export const pca = (x, y) => {
 
     const pca = new ML.PCA(dataset)
 }
@@ -51,4 +52,49 @@ export const linReg = (x, y) => {
     const model = predictor => regression.predict(predictor) //returns regression result based on predictor
 
     return model
+}
+
+export const trainNN = (data) => {
+
+    const options = {
+        task: 'classification',
+        debug: true
+    }
+
+    const nn = ml5.neuralNetwork(options);
+
+    data.forEach(sample => {
+
+        const input = [] // input data
+        const output = []// classification
+        nn.addData(input, output)
+
+    })
+
+
+    nn.normalizeData()
+
+    const trainingOptions = {
+        epochs: 32,
+        batchSize: 12
+    }
+    nn.train(trainingOptions, finishedTraining);
+    
+}
+
+function handleResults(error, result) {
+    if(error){
+      console.error(error);
+      return;
+    }
+    console.log(result); // {label: 'red', confidence: 0.8};
+}
+
+export const testNN = (data) => {
+
+    const inputs; // testing data
+    nn.classify(inputs, handleResults);
+
+    // stuff to calculate accuracy?
+
 }
